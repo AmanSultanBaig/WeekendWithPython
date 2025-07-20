d621bdcbdbc5a29183b2a21d6a825a049d3dd883
Aman Sultan Baig <amansultanbaig@gmail.com>
2025-07-20 22:18:35 +0500

    built in function in python of strings

---
+++ built-in-functions/function.py
@@ -0,0 +1,6 @@
+course_name = "Python Programming"
+
+print(len(course_name))  # output 18 length of string
+print(course_name[0])    # output P
+print(course_name[-1])   # output g | get string char from desc order
+print(course_name[0:6])  # output Python | slice operation
