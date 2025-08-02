5da658b96a279ae82eae5c22139dc7d42d497ebc
Aman Sultan Baig <amansultanbaig@gmail.com>
2025-08-02 23:31:38 +0500

    numbers methond of python in weekend

---
+++ numbers/numbers.py
@@ -0,0 +1,20 @@
+import math
+
+print(10 + 3)
+print(10 - 3)
+print(10 * 3)
+print(10 / 3)
+print(10 // 3)  # this will skip decimal values
+print(10 % 3)
+print(10 ** 3)
+
+x = 3
+x = x + 3
+x += 3
+
+print(x)
+
+print("---------Number Methods-----------\n")
+print(round(2.9))
+print(abs(-2.9))
+print(math.ceil(2.9))
