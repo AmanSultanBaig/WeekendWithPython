d2c21eec51ede2b02afb84234f745e1c2ea93b80
Aman Sultan Baig <amansultanbaig@gmail.com>
2025-08-02 23:18:05 +0500

    string methods on weekend

---
+++ string-methods/string-methods.py
@@ -0,0 +1,27 @@
+course = "    Python Programming"
+
+print(len(course))
+print("-------------------")
+print(course.islower())
+print("-------------------")
+print(course.isupper())
+print("-------------------")
+print(course.istitle())
+print("-------------------")
+
+
+print(course.upper())
+print("-------------------")
+print(course.lower())
+print("-------------------")
+print(course.title())
+print("-------------------")
+print(course.strip())  # remove wide spaces
+print("-------------------")
+print(course.find("Pro"))  # find the index of given string
+print("-------------------")
+print(course.replace("Pro", "Jro"))
+print("-------------------")
+print("Pro" in course)
+print("-------------------")
+print("Java" not in course)
